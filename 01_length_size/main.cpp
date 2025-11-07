60b3232beb480860e2148d25511d05f3ef394a38
tetukokukilowangsit-droid <tetukokukilowangsit@gmail.com>
2025-11-07 14:42:02 +0700

    Add string length and size demonstration
    Prints the length and size of a string.

---
+++ 01_length_size/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main(){
+  string kata = "SMKN 1 BOYOLANGU";
+  int katalen = kata.length();
+  int katasize = kata.size();
+  cout << kata<<endl;
+  cout << "kata length = "<<kata<<endl;
+  cout << "kata size = " << katasize <<endl;
+}
